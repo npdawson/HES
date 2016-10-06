d706eabf3dbfdf5baa59a036e42710ae42ed15d0
Nathaniel P. Dawson <nathaniel.dawson@gmail.com>
2016-10-06 07:28:58 -0700

    refactored CPU monad stack a bit

--- src/Cpu.hs
+++ src/Cpu.hs
@@ -1,23 +1,46 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Cpu where
 
 -- import Instruction
 import Control.Monad.Reader
-import Control.Monad.ST
-import Data.STRef
+import Control.Monad.State
+import qualified Data.ByteString as B
 import qualified Data.Vector.Unboxed.Mutable as VM
-import qualified Data.Vector.Unboxed as V
 import Data.Word
 
-data CpuState s = CpuState
-  {
-    regA :: STRef s Word8
-  , regX :: STRef s Word8
-  , regY :: STRef s Word8
-  , pc   :: STRef s Word16
-  , sp   :: STRef s Word8
-  , regP :: STRef s Word8
-  , rom  :: V.Vector Word8
-  , ram  :: VM.MVector s Word8
+data CpuConfig = CpuConfig
+  { rom  :: B.ByteString -- TODO create Cart type with mapper functionality
+    -- ram is only a reference to a mutable vector, so it can be
+    -- passed around in the Reader monad
+  , ram  :: VM.IOVector Word8
   }
 
-type CPU s = ReaderT () (ST s)
+data CpuState = CpuState
+  { regA :: Word8
+  , regX :: Word8
+  , regY :: Word8
+  , regP :: Word8
+  , pc   :: Word16
+  , sp   :: Word8
+  }
+
+newtype CPU a = CPU { runC :: ReaderT CpuConfig (StateT CpuState IO) a }
+  deriving ( Functor, Applicative, Monad, MonadIO
+           , MonadReader CpuConfig, MonadState CpuState )
+
+runCPU :: CPU a -> B.ByteString -> IO (a, CpuState)
+runCPU cpu rom0 = do
+  ram0 <- VM.replicate 65536 0
+  let config = CpuConfig
+        { rom = rom0
+        , ram = ram0
+        }
+      initState = CpuState
+        { regA = 0
+        , regX = 0
+        , regY = 0
+        , regP = 0
+        , pc = 0
+        , sp = 0
+        }
+    in runStateT (runReaderT (runC cpu) config) initState
